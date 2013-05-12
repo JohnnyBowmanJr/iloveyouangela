04def7f5a729caa513b43157601e06146dc72ede
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-12 01:46:44 -0400

    init git

---
+++ app/assets/javascripts/reasons.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	reasons = ["Because you support me in everything I do.",
+								"Because you're there when I need you most",
+								"Because your french toast is still the best",
+								"Because most of my clothes are still ones you've picked out for me",
+								"Because you always have my back",
+								"Because of all the piano lessons, tennis lessons, drum lessons, and life lessons that've made me who I am",
+								"Because I secretly love the ballet"
+								];
+
+	buttonText = ["Obviously",
+								"I knew that",
+								"Well I suppose so",
+								"Keep it coming",
+								"Mmmmmmhhhmmmm"
+								];
+
+	var randomReason = Math.floor(Math.random() * reasons.length);
+	var randombuttonText = Math.floor(Math.random() * buttonText.length);
+	$('#reason').append(reasons[randomReason]);
+	$('button#inquire').append(buttonText[randombuttonText]);
+
+	$('body').on("click", "#inquire", function() {
+		var randomReason = Math.floor(Math.random() * reasons.length);
+		var randombuttonText = Math.floor(Math.random() * buttonText.length);
+		$('#reason').replaceWith("<div id='reason'>" + reasons[randomReason] + "</div>");
+		$('button#inquire').replaceWith("<button id='inquire'>" + buttonText[randombuttonText] + "</button>");
+		console.log(randomReason);
+	});
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mothersday::Application.config.secret_token = 'c24252203db869f3e3537fc7dbc476a07ca3af5d9c46d94e7c36ece4c8bc455150431f19ca197941d3d0a15e02a8ba2bac5af6c1b53891bb07038c03eaf22109'
